3d2c4dce261de9deb9ec7af88104c3d8639e9c0d
naru-jpn <tus.naru@gmail.com>
2016-05-26 20:20:12 +0900

    Add Support Carthage

---
+++ Sources/StructArchiver.h
@@ -0,0 +1,12 @@
+//
+//  StructArchiver.h
+//  StructArchiver
+//
+//  Created by naru on 2016/05/26.
+//  Copyright © 2016年 naru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double StructArchiverVersionNumber;
+FOUNDATION_EXPORT const unsigned char StructArchiverVersionString[];
